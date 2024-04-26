62b4d157956faf4bdb2692e008206083fb9a0e38
Ann <annchristy@live.com>
2024-04-25 21:35:42 -0400

    here we go again

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-const { getModuleBuildInfo } = require('next/dist/build/webpack/loaders/get-module-build-info');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //reactStrictMode: true,
-  output:'standalone',
+  reactStrictMode: true,
+
 }
 
 module.exports = nextConfig
