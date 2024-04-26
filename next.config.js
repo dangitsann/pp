25333233b3fdff36d335fe402bdb8a062a3588ba
Ann <annchristy@live.com>
2024-04-25 21:17:01 -0400

    fix export statement in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+const { getModuleBuildInfo } = require('next/dist/build/webpack/loaders/get-module-build-info');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //reactStrictMode: true,
   output:'export',
 }
 
-/* module.exports = nextConfig */
-export default nextConfig;
+module.exports = nextConfig
